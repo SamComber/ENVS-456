4d0f17f6ce393ed8da2d01b0dfd468b18a873bb5
sam comber <sam.comber@hotmail.co.uk>
2017-02-07 14:05:27 +0000

    data done

--- server.R
+++ server.R
@@ -19,8 +19,9 @@ server <- function(input, output, session) {
   # build lat/lon + date string to send with postrequest
   curl.string <- paste0('poly=',paste0(sprintf('%s,%s',lpool[,2], lpool[,1]), collapse = ':'))
   
-  dates = c("2015-12", "2016-01", "2016-02", "2016-03", "2016-04", "2016-05", "2016-06", "2016-07", "2016-08", "2016-09", "2016-10", "2016-11")
-
+  # dates = c("2015-12", "2016-01", "2016-02", "2016-03", "2016-04", "2016-05", "2016-06", "2016-07", "2016-08", "2016-09", "2016-10", "2016-11")
+  dates = c("2015-12", "2016-01")
+  
   document <- lapply(dates, function(month) {
     # format acceptable packet for http request
     curl.string <- list(poly=c(curl.string), date=c(month))
@@ -30,27 +31,25 @@ server <- function(input, output, session) {
     jsonlite::fromJSON(txt=json)
   })
   
-  master <- data.frame(id=numeric(0), category=character(0), latitude=character(0), longitude=character(0), month=character(0), outcome_status=character(0))
-  d1 <- data.frame(category=document[[1]]$category,lon=document[[1]]$location$latitude, lat=document[[1]]$location$longitude, id=document[[1]]$id, name=document[[1]]$location$street$name, month=document[[1]]$month, outcome_status=document[[1]]$outcome_status$category)
-  d2 <- data.frame(category=document[[2]]$category,lon=document[[2]]$location$latitude, lat=document[[2]]$location$longitude, id=document[[2]]$id, name=document[[2]]$location$street$name, month=document[[2]]$month, outcome_status=document[[2]]$outcome_status$category)
-  d3 <- data.frame(category=document[[3]]$category,lon=document[[3]]$location$latitude, lat=document[[3]]$location$longitude, id=document[[3]]$id, name=document[[3]]$location$street$name, month=document[[3]]$month, outcome_status=document[[3]]$outcome_status$category)
-  d4 <- data.frame(category=document[[4]]$category,lon=document[[4]]$location$latitude, lat=document[[4]]$location$longitude, id=document[[4]]$id, name=document[[4]]$location$street$name, month=document[[4]]$month, outcome_status=document[[4]]$outcome_status$category)
-  d5 <- data.frame(category=document[[5]]$category,lon=document[[5]]$location$latitude, lat=document[[5]]$location$longitude, id=document[[5]]$id, name=document[[5]]$location$street$name, month=document[[5]]$month, outcome_status=document[[5]]$outcome_status$category)
-  d6 <- data.frame(category=document[[6]]$category,lon=document[[6]]$location$latitude, lat=document[[6]]$location$longitude, id=document[[6]]$id, name=document[[6]]$location$street$name, month=document[[6]]$month, outcome_status=document[[6]]$outcome_status$category)
-  d7 <- data.frame(category=document[[7]]$category,lon=document[[7]]$location$latitude, lat=document[[7]]$location$longitude, id=document[[7]]$id, name=document[[7]]$location$street$name, month=document[[7]]$month, outcome_status=document[[7]]$outcome_status$category)
-  d8 <- data.frame(category=document[[8]]$category,lon=document[[8]]$location$latitude, lat=document[[8]]$location$longitude, id=document[[8]]$id, name=document[[8]]$location$street$name, month=document[[8]]$month, outcome_status=document[[8]]$outcome_status$category)
-  d9 <- data.frame(category=document[[9]]$category,lon=document[[9]]$location$latitude, lat=document[[9]]$location$longitude, id=document[[9]]$id, name=document[[9]]$location$street$name, month=document[[9]]$month, outcome_status=document[[9]]$outcome_status$category)
-  d10 <- data.frame(category=document[[10]]$category,lon=document[[10]]$location$latitude, lat=document[[10]]$location$longitude, id=document[[10]]$id, name=document[[10]]$location$street$name, month=document[[10]]$month, outcome_status=document[[10]]$outcome_status$category)
-  d11 <- data.frame(category=document[[11]]$category,lon=document[[11]]$location$latitude, lat=document[[11]]$location$longitude, id=document[[11]]$id, name=document[[11]]$location$street$name, month=document[[11]]$month, outcome_status=document[[11]]$outcome_status$category)
-  d12 <- data.frame(category=document[[12]]$category,lon=document[[12]]$location$latitude, lat=document[[12]]$location$longitude, id=document[[12]]$id, name=document[[12]]$location$street$name, month=document[[12]]$month, outcome_status=document[[12]]$outcome_status$category)
+  master <- data.frame(id=numeric(0), category=character(0), lat=character(0), lon=character(0), month=character(0), outcome_status=character(0))
+  d1 <- data.frame(category=document[[1]]$category,lat=document[[1]]$location$latitude, lon=document[[1]]$location$longitude, id=document[[1]]$id, name=document[[1]]$location$street$name, month=document[[1]]$month, outcome_status=document[[1]]$outcome_status$category)
+  d2 <- data.frame(category=document[[2]]$category,lat=document[[2]]$location$latitude, lon=document[[2]]$location$longitude, id=document[[2]]$id, name=document[[2]]$location$street$name, month=document[[2]]$month, outcome_status=document[[2]]$outcome_status$category)
+  # d3 <- data.frame(category=document[[3]]$category,lat=document[[3]]$location$latitude, lon=document[[3]]$location$longitude, id=document[[3]]$id, name=document[[3]]$location$street$name, month=document[[3]]$month, outcome_status=document[[3]]$outcome_status$category)
+  # d4 <- data.frame(category=document[[4]]$category,lat=document[[4]]$location$latitude, lon=document[[4]]$location$longitude, id=document[[4]]$id, name=document[[4]]$location$street$name, month=document[[4]]$month, outcome_status=document[[4]]$outcome_status$category)
+  # d5 <- data.frame(category=document[[5]]$category,lat=document[[5]]$location$latitude, lon=document[[5]]$location$longitude, id=document[[5]]$id, name=document[[5]]$location$street$name, month=document[[5]]$month, outcome_status=document[[5]]$outcome_status$category)
+  # d6 <- data.frame(category=document[[6]]$category,lat=document[[6]]$location$latitude, lon=document[[6]]$location$longitude, id=document[[6]]$id, name=document[[6]]$location$street$name, month=document[[6]]$month, outcome_status=document[[6]]$outcome_status$category)
+  # d7 <- data.frame(category=document[[7]]$category,lat=document[[7]]$location$latitude, lon=document[[7]]$location$longitude, id=document[[7]]$id, name=document[[7]]$location$street$name, month=document[[7]]$month, outcome_status=document[[7]]$outcome_status$category)
+  # d8 <- data.frame(category=document[[8]]$category,lat=document[[8]]$location$latitude, lon=document[[8]]$location$longitude, id=document[[8]]$id, name=document[[8]]$location$street$name, month=document[[8]]$month, outcome_status=document[[8]]$outcome_status$category)
+  # d9 <- data.frame(category=document[[9]]$category,lat=document[[9]]$location$latitude, lon=document[[9]]$location$longitude, id=document[[9]]$id, name=document[[9]]$location$street$name, month=document[[9]]$month, outcome_status=document[[9]]$outcome_status$category)
+  # d10 <- data.frame(category=document[[10]]$category,lat=document[[10]]$location$latitude, lon=document[[10]]$location$longitude, id=document[[10]]$id, name=document[[10]]$location$street$name, month=document[[10]]$month, outcome_status=document[[10]]$outcome_status$category)
+  # d11 <- data.frame(category=document[[11]]$category,lat=document[[11]]$location$latitude, lon=document[[11]]$location$longitude, id=document[[11]]$id, name=document[[11]]$location$street$name, month=document[[11]]$month, outcome_status=document[[11]]$outcome_status$category)
+  # d12 <- data.frame(category=document[[12]]$category,lat=document[[12]]$location$latitude, lon=document[[12]]$location$longitude, id=document[[12]]$id, name=document[[12]]$location$street$name, month=document[[12]]$month, outcome_status=document[[12]]$outcome_status$category)
+  document <- rbind(master, d1, d2)
+  # document <- rbind(master, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12)
   
-  document <- rbind(master, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12)
+  # fix bug where columns are converted to factors. Set as chr class 
+  document <- rapply(document, as.character, classes="factor", how="replace") 
   
-  
-  
-  
-
-
   
   # ---------- DATA CLEANING -----------
   
@@ -76,11 +75,11 @@ server <- function(input, output, session) {
                      Sys.sleep(0.25)
                    }
                    
-                   leaflet(data) %>% addTiles() %>% addCircles(lng = as.numeric(document$location$longitude), lat = as.numeric(document$location$latitude), color = pal(document$category), weight =1, 
+                   leaflet() %>% addTiles() %>% addCircles(lng = as.numeric(document$lon), lat = as.numeric(document$lat), color = pal(document$category), weight =1, 
                                                            popup =   paste("<b>Crime:</b> ", document$category, "<br>",
                                                                            "<b>Date:</b> <i>", document$month, "</i><br>",
-                                                                           "<b>Location:</b> ", document$location$street$name, "<br>", 
-                                                                           "<b>Outcome:</b> ", document$outcome_status$category, "<br>")) %>% addLegend("bottomleft", values = document$category, pal = pal)
+                                                                           "<b>Location:</b> ", document$name, "<br>", 
+                                                                           "<b>Outcome:</b> ", document$outcome_status, "<br>")) %>% addLegend("bottomleft", values = document$category, pal = pal)
                  })
   })
   
@@ -92,7 +91,7 @@ server <- function(input, output, session) {
   
   output$crimetable <- DT::renderDataTable({
     # subset crimes table 
-    new.document <- data.frame(document$category, document$location$latitude, document$location$longitude, document$location$street$name ,document$month)
+    new.document <- data.frame(document$category, document$lat, document$lon, document$name ,document$month)
     # rename columns
     names(new.document) <- c("Category", "Latitude", "Longitude", "Location", "Month")
     
