1b944542adb510f40d09588fe884eb366b8e8898
sam comber <sam.comber@hotmail.co.uk>
2017-02-13 14:02:32 +0000

    doot

--- server.R
+++ server.R
@@ -33,9 +33,11 @@ server <- function(input, output, session) {
     # post custom polygon to police api (note: post needed as appending curl string to url is too long)
     r <- httr::POST("https://data.police.uk/api/crimes-street/all-crime", body = curl.string, encode="multipart", verbose())
     json <- content(r, "text")
+    # return as data.frame
     jsonlite::fromJSON(txt=json)
   })
   
+  # build master data.frame to append data.frame for individual months to
   master <- data.frame(id=numeric(0), category=character(0), lat=character(0), lon=character(0), month=character(0), outcome_status=character(0))
   d1 <- data.frame(category=document[[1]]$category,lat=document[[1]]$location$latitude, lon=document[[1]]$location$longitude, id=document[[1]]$id, name=document[[1]]$location$street$name, month=document[[1]]$month, outcome_status=document[[1]]$outcome_status$category)
   d2 <- data.frame(category=document[[2]]$category,lat=document[[2]]$location$latitude, lon=document[[2]]$location$longitude, id=document[[2]]$id, name=document[[2]]$location$street$name, month=document[[2]]$month, outcome_status=document[[2]]$outcome_status$category)
@@ -50,10 +52,10 @@ server <- function(input, output, session) {
   d11 <- data.frame(category=document[[11]]$category,lat=document[[11]]$location$latitude, lon=document[[11]]$location$longitude, id=document[[11]]$id, name=document[[11]]$location$street$name, month=document[[11]]$month, outcome_status=document[[11]]$outcome_status$category)
   d12 <- data.frame(category=document[[12]]$category,lat=document[[12]]$location$latitude, lon=document[[12]]$location$longitude, id=document[[12]]$id, name=document[[12]]$location$street$name, month=document[[12]]$month, outcome_status=document[[12]]$outcome_status$category)
   # document <- rbind(master, d1, d2)
+  
+  # rbind each month to master data.frame
   document <- rbind(master, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12)
   
-  # document <- document[sample(nrow(document), 10000), ]
-
   # fix bug where columns are converted to factors. Set as chr class
   document <- rapply(document, as.character, classes="factor", how="replace")
   # recode month to integer value for sliderInput functionality
@@ -81,20 +83,25 @@ server <- function(input, output, session) {
 
   # ---------- DATA PROCESSING ----------
 
+  # choose palette colour
   pal <- colorFactor(
     palette = "YlGnBu",
     domain = document$category
   )
 
+  # render static leaflet map without dynamically-loaded data
   output$map <- renderLeaflet({
 
+    # create progress bar for rendering map output
     withProgress(message = 'Rendering Map Data',
                  detail = 'this may take a few seconds...', value = 0, {
+                   # increment progress bar 
                    for (i in 1:60) {
                      incProgress(1/60)
                      Sys.sleep(0.25)
                    }
 
+                   # define static attrbutes of leaflet output
                    leaflet() %>% addProviderTiles("CartoDB.Positron")  %>%
                      fitBounds(-3.008756, 53.32679, -2.818, 53.47497) 
                    # %>% addLegend("bottomleft", values = document$category, pal = pal)
@@ -103,28 +110,36 @@ server <- function(input, output, session) {
   
   # ---------- REACTIVE DATA -----------
   
-  # react.document <- reactive({
-  #   document[sample(nrow(document), input$samplesize), ]
-  # })
+  # reactively choose sample size based on slider input
+  react.document <- reactive({
+    document[sample(nrow(document), input$samplesize), ]
+  })
   
+  # reactively choose crime data to show based on date range chosen by input slider
   time.document <- reactive({
     document[document$intmonth >= input$time[1] & document$intmonth <= input$time[2], ]
   })
   
   # observe({
-  #   leafletProxy("map", data = react.document()) %>% addCircleMarkers(as.numeric(react.document()$lon),
-  #                                                                     as.numeric(react.document()$lat),
-  #                                                                     group = "Markers",
-  #                                                                     radius = 5,
-  #                                                                     clusterOptions = markerClusterOptions(),
-  #                                                                     popup = paste("<b>Crime:</b> ", document$category, "<br>",
-  #                                                                                  "<b>Date:</b> <i>", document$month, "</i><br>",
-  #                                                                                  "<b>Location:</b> ", document$name, "<br>",
-  #                                                                                  "<b>Outcome:</b> ", document$outcome_status, "<br>"))
+  #   # clearMarkerClusters upon changing slider
+  #   if(nrow(time.document()) == 0) {
+  #     leafletProxy("map", data = react.document()) %>% clearMarkerClusters()
+  #   } else {
+  #     leafletProxy("map", data = react.document()) %>% addCircleMarkers(as.numeric(react.document()$lon),
+  #                                                                       as.numeric(react.document()$lat),
+  #                                                                       group = "Markers",
+  #                                                                       radius = 5,
+  #                                                                       clusterOptions = markerClusterOptions(),
+  #                                                                       popup = paste("<b>Crime:</b> ", document$category, "<br>",
+  #                                                                                     "<b>Date:</b> <i>", document$month, "</i><br>",
+  #                                                                                     "<b>Location:</b> ", document$name, "<br>",
+  #                                                                                     "<b>Outcome:</b> ", document$outcome_status, "<br>"))
+  #   }
   # })
   
+  # create obserer object to re-execute everytime reactive data input is changed - i.e. slider inputs
   observe({
-    # clearMarkerClusters 
+    # ensure markers are cleared each time observer re-executes to elininate data duplication
     if(nrow(time.document()) == 0) {
       leafletProxy("map", data = time.document()) %>% clearMarkerClusters()
     } else {
@@ -133,20 +148,18 @@ server <- function(input, output, session) {
                                                                                                  group = "Markers",
                                                                                                  radius = 5,
                                                                                                  clusterOptions = markerClusterOptions(),
+                                                                                                 # build popup markers on individual crimes
                                                                                                  popup = paste("<b>Crime:</b> ", document$category, "<br>",
                                                                                                                "<b>Date:</b> <i>", document$month, "</i><br>",
                                                                                                                "<b>Location:</b> ", document$name, "<br>",
                                                                                                                "<b>Outcome:</b> ", document$outcome_status, "<br>"))  
     }
-    
-    
   })
   
-
-
+  # line graph showing time series of aggregated crime counts per month
   output$crimeline <- renderPlot({
-    # summate crimes per month
-    t <- count(document, "month")
+    # summate crimes per month using reactive data
+    t <- count(react.document(), "month")
     ggplot(t, aes(month, freq, group = 1)) +
       geom_point(colour = '#496D64') +
       geom_line(colour = '#496D64') + theme_bw() +
@@ -155,13 +168,15 @@ server <- function(input, output, session) {
       theme(axis.text.x=element_text(angle=90, hjust=1))
   })
 
+  # use reactive data to render epsilon optimisation plot
   output$dbopt <- renderPlot({
-    x <- data.frame(lat=as.numeric(document$lat), lon=as.numeric(document$lon))
+    x <- data.frame(lat=as.numeric(react.document()$lat), lon=as.numeric(react.document()$lon))
     dbscan::kNNdistplot(x, k = 10)
   })
 
+  # render dbscan using reactive data
   output$dbscan <- renderPlot({
-    x <- data.frame(lat=as.numeric(document$lat), lon=as.numeric(document$lon))
+    x <- data.frame(lat=as.numeric(react.document()$lat), lon=as.numeric(react.document()$lon))
     res.db <- dbscan::dbscan(x, 0.0015, 5)
     plot(res.db, x,
          frame = FALSE,
@@ -170,20 +185,21 @@ server <- function(input, output, session) {
          ylab="Longitude")
   })
   
+  # create bar chart showing crime frequencies per category
   output$crimebar <- renderPlot({
-    counts <- data.frame(table(document$category))
+    counts <- data.frame(table(react.document()$category))
     row.names(counts) <- counts$Var1
-    par(mar = c(7, 4, 2, 2) + 0.2) #add room for the rotated labels
+    #add room for the rotated labels
+    par(mar = c(7, 4, 2, 2) + 0.2) 
 
     barplot(counts$Freq, xlab="", ylab="Frequency", space=1)
-    end_point = 0.5 + nrow(counts) + nrow(counts)-1 #this is the line which does the trick (together with barplot "space = 1" parameter)
+    end_point = 0.5 + nrow(counts) + nrow(counts)-1 
 
+    # ad rotated labels for aesthetical plot
     text(seq(1.5,end_point,by=2), par("usr")[3]-0.25,
          srt = 60, adj= 1, xpd = TRUE,
          labels = paste(rownames(counts)), cex=1)
-    
-    # counts %>% mjs_plot(x=Freq, y=Var1, width=500, height=400) %>% mjs_bar() %>% mjs_axis_x(xax_format = 'plain')    
-  })
+    })
 
   output$crimetable <- DT::renderDataTable({
     # subset crimes table
@@ -191,9 +207,10 @@ server <- function(input, output, session) {
     # rename columns
     names(new.document) <- c("Category", "Latitude", "Longitude", "Location", "Outcome", "Month")
 
-    # df <- document %>% filter()
+    # create ajax table ajax that allows filtering operations without page reloads
     action <- DT::dataTableAjax(session, new.document)
 
+    # initialise AJAX-loaded table
     DT::datatable(new.document, options = list(ajax = list(url = action)), escape=FALSE)
   })
   
--- ui.R
+++ ui.R
@@ -28,7 +28,7 @@ ui <- navbarPage("Crime Mapping in Liverpool", theme = shinytheme("flatly"), id=
                           h2("Data controller"),
                           sliderInput("time", "Month Slider", min = 1, max = 12, value = c(1,12), step=1, pre = "Month: "),
                           plotOutput("crimeline", height = 200),
-                          sliderInput("samplesize", "Sample Size", min = 1000, max=10000, value = 10000, step = 1000),
+                          sliderInput("samplesize", "Sample Size", min = 10000, max=60000, value = 10000, step = 10000),
                           p(class="text-muted", "Note: pulling data from the Police API may take a few minutes...")
                           )
 
@@ -42,11 +42,11 @@ ui <- navbarPage("Crime Mapping in Liverpool", theme = shinytheme("flatly"), id=
                       ),
                       
                       column(4,
-                        h4("DBSCAN - epsilson = 0.002, min points = 10, n = 68793"),
+                        h4("DBSCAN - epsilson = 0.002, min points = 10"),
                         plotOutput("dbscan")
                       ),
                       column(4,
-                        h4("Crime category frequencies (n = 68793)"),
+                        h4("Crime category frequencies"),
                         plotOutput("crimebar")
                       ),
                       hr()
@@ -60,6 +60,18 @@ ui <- navbarPage("Crime Mapping in Liverpool", theme = shinytheme("flatly"), id=
                       a user to filter a location by road name to count the number of crimes."),
                     DT::dataTableOutput("crimetable"),
                     hr()
-                    )
+           ),
+           tabPanel("About",
+                    p("This shiny app is the product of a class project for ENVS456 - Web Mapping and Analysis designed by Sam Comber. Cluster markers provided
+                      by the Leaflet library are used to lower the overhead of plotting 64,000+ individual crime points. By zooming into or clicking each cluster,
+                      the user is able to visualise the geography of crime density across Liverpool in their viewport. Crime clusters can be expanded until their
+                      constituent crimes are shown individually (clicking individual crimes shows a popup detailing descriptive information). As there is no server-side
+                      backend for storing crimes, each page refresh requires a new HTTP request to the Police API which significantly enhances the client-side loadtime of the app.
+                      For this reason, we use a single-page application (SPA) design that uses AJAX to eliminate the need for page refreshes. The data explorer tab tabulates the
+                      tidied dataset, allowing the user to search by location, outcome status and more."),
+                    p("Moreover, by using the interactive sliders, the user is able to observe how the spatio-temporal variation in crime density.
+                      Note: the sample size slider does not change the number of crime points visualised on the map. The Github repository for the project
+                      can be found: ", a("here", href="https://github.com/SamComber"), " and the R markdown file that explains the design process ", a("here.", href="https://github.com/SamComber"))
+           )
            
 )
